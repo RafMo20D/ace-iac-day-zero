1cc2f5d414a395425116d70e013fae057bd943d0
Mohamed Ahmed <softtechsd@gmail.com>
2023-10-24 02:26:12 +0300

    add

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 3.1.1"
+      version = "~> 2.24.1"
     }
     aws = {
       source  = "hashicorp/aws"
